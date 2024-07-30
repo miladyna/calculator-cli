0470a25e45770a57c2640e0a8996623578a4f138
Mileidy Vanegas Martinez <vanegasmartinezmileidy@gmail.com>
2024-07-30 13:33:02 -0500

    Create Calculator class with the add method

---
+++ calculator/model.py
@@ -0,0 +1,7 @@
+class Calculator:
+    
+    def  add(self, num_1: float, num_2: float) -> float:
+        return num_1 + num_2
+
+    def  add(self, num_1: float, num_2: float) -> float:
+        return num_1 - num_2
