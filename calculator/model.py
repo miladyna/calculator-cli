ff910945b71636e161f8e7684d8e42f7400d48b2
Mileidy Vanegas Martinez <vanegasmartinezmileidy@gmail.com>
2024-07-30 13:54:32 -0500

    class

--- calculator/model.py
+++ calculator/model.py
@@ -3,5 +3,14 @@ class Calculator:
     def  add(self, num_1: float, num_2: float) -> float:
         return num_1 + num_2
 
-    def  add(self, num_1: float, num_2: float) -> float:
+    def  subtract(self, num_1: float, num_2: float) -> float:
         return num_1 - num_2
+
+    def  multiply(self, num_1: float, num_2: float) -> float:
+        return num_1 * num_2
+
+    def  divide(self, num_1: float, num_2: float) -> float:
+        if num_2 == 0:
+            return "Cannot divide by 0!"
+        else:
+            return num_1 / num_2
